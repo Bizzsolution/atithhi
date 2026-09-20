4c0a4361f7dc6406de55dcb5fb7070664c5b3b4d
Bizzsolution <bizsaathi5@gmail.com>
2026-09-20 18:28:01 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -5,7 +5,7 @@
 // shell forever, even after a fresh deploy (exactly what caused devices
 // to keep showing an outdated version despite redeploying).
 
-const CACHE = 'atithibook-v4.9';
+const CACHE = 'atithibook-v5.3';
 const OFFLINE_URLS = [
   '/',
   '/index.html',
